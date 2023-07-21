659951b7a6f68a40900a294a88e3e4582e3314c5
Marcela Brandao <marcela.brandao@mira.eti.br>
2023-07-21 18:01:58 -0300

    first commit project

--- src/app/escopodoc/page.tsx
+++ src/app/escopodoc/page.tsx
@@ -1,10 +1,9 @@
 
 
-export default function Home () {
+export default function EscopoDoc () {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-
-
+      OLÁ AMIGOS DO PRESUNTINHO na page DOCUMENTATION
     </main>
   )
 }
---
+++ src/componentes/Header/index.tsx
@@ -0,0 +1,7 @@
+export default function Header () {
+  return (
+    <header className="flex text-red-300">
+      <p className="text-red-600">header da porra toda</p>
+    </header>
+  )
+}
